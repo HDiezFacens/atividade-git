4684a589ee8c123eafe827688565cf733fe8b777
Henry <hdiezfacens@gmail.com>
2026-08-18 20:23:47 -0300

    Atualiza projeto

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 let contador = 0;
 
-document.getElementById("botao").onclick= () => {
+document.getElementById("botao").onclick = () => {
     contador++;
-    let toast = document.getElementById("toast");
-    if(!toast) {
-        toast = document.createElement("div");
-        toast.id = "toast";
-        toast.className="toast";
-        document.body.appendChild(toast);
-    }
-    toast.textContent= `Você clicou ${contador}`;
-    toast.classList.add("show");
+
+    document.getElementById("contadorTexto").textContent =
+        `Você clicou ${contador} vez${contador === 1 ? "" : "es"}!`;
 };
